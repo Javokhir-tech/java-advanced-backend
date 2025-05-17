d648137bbd2aeff031cb53838800d6a1529e7c79
JavokhirAbdullaev <JAbdullaev-c@scai.sa>
2025-05-17 17:16:25 +0500

    Task 2 - CRUD Rest app

---
+++ src/main/java/com/advanced/backend/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.advanced.backend.repository;
+
+
+import com.advanced.backend.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {}
