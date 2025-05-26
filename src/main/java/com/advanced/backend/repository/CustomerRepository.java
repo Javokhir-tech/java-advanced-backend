b6d41ff3309509cffb15ea54bc66ca883367ce57
JavokhirAbdullaev <JAbdullaev-c@scai.sa>
2025-05-26 19:06:36 +0500

    advanced backend spring boot

--- src/main/java/com/advanced/backend/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.advanced.backend.repository;
-
-
-import com.advanced.backend.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {}
